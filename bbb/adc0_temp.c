3eafcf24ea9d507e87430be163148b181a56c3a9
Dhruva Gole <goledhruva@gmail.com>
2021-11-04 21:28:18 +0530

    Add time stamp to each reading
    Signed-off-by: Dhruva Gole <goledhruva@gmail.com>

--- bbb/adc0_temp.c
+++ bbb/adc0_temp.c
@@ -1,41 +1,45 @@
 #include <stdio.h>      // for printf()
 #include <unistd.h>     // for sleep()
 #include <stdlib.h>
+#include <time.h>
 
-#define SECONDS 1000000	
+#define SECONDS 1000000
 // 1 sec = 1000000 usec
 
 int main() {
-	unsigned long int reading_count = 0;
-	FILE *fp_temp, *fp_adc, *fp_write;
-	char buff[8];
+        time_t local_time;
+        unsigned long int reading_count = 0;
+        FILE *fp_temp, *fp_adc, *fp_write;
+        char buff[8];
 
-	while(1) {
-		++reading_count;
-		fp_adc = fopen("/sys/bus/iio/devices/iio:device0/in_voltage0_raw", "r");    // voltageP9_39 -> /sys/bus/iio/devices/iio:device0/in_voltage0_raw
-		fscanf(fp_adc, "%s", buff);
-		printf("\n{--reading: %ld--}\nvalue : %s\n", reading_count, buff );
-		fp_write = fopen("bat_read1.txt", "aw");
+        while(1) {
+                time(&local_time);
+                ++reading_count;
+                fp_adc = fopen("/sys/bus/iio/devices/iio:device0/in_voltage0_raw", "r");    // voltageP9_39 -> /sys/bus/iio/devices/iio:device0/in_voltage0_raw
+                fscanf(fp_adc, "%s", buff);
+                printf("\n---\nTime:%s", ctime(&local_time));
+                printf("\n{--reading: %ld--}\nvoltage : %s\n", reading_count, buff );
+                fp_write = fopen("4NOV-bat_read1.txt", "aw");
 
-		/* Write data to file */
-		fprintf(fp_write, "\n{--reading: %ld--}\n{voltage : %s}", reading_count, buff );
-		/* Close file to save file data */
-		fclose(fp_write);
+                /* Write data to file */
+                fprintf(fp_write, "\n---\nTime:%s\n{--reading: %ld--}\n{voltage : %s}", ctime(&local_time), reading_count, buff );
+                /* Close file to save file data */
+                fclose(fp_write);
 
-		fp_temp = fopen("/sys/class/hwmon/hwmon0/temp1_input", "r");
-		fscanf(fp_temp, "%s", buff);
-		printf("temperature = %s\n", buff );
+                fp_temp = fopen("/sys/class/hwmon/hwmon0/temp1_input", "r");
+                fscanf(fp_temp, "%s", buff);
+                printf("temperature = %s\n", buff );
 
-		fp_write = fopen("bat_read1.txt", "aw");
+                fp_write = fopen("4NOV-bat_read1.txt", "aw");
 
-		/* Write data to file */
-		fprintf(fp_write, "\n{temperature: %s}\n", buff);
-		/* Close file to save file data */
-		fclose(fp_write);
-		
-		usleep(1.5*SECONDS);
-		fclose(fp_temp);
-		fclose(fp_adc);
-	}
-	return EXIT_SUCCESS;
+                /* Write data to file */
+                fprintf(fp_write, "\n{temperature: %s}\n", buff);
+                /* Close file to save file data */
+                fclose(fp_write);
+
+                usleep(1*SECONDS);
+                fclose(fp_temp);
+                fclose(fp_adc);
+        }
+        return EXIT_SUCCESS;
 }
